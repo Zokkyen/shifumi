1eab7963d598d0bac8e548f893f0726aa7a173bc
Zokkyen <stephane.swaenepoel@wanadoo.fr>
2022-06-20 18:26:56 +0200

    little maj

--- assets/js/scriptV2.js
+++ assets/js/scriptV2.js
@@ -4,7 +4,67 @@ let victoryUserCount = 0;
 let victoryCompCount = 0;
 let nbEgalityCount = 0;
 let nbGameCount = 0;
-let alwaysOne = 0;
+
+/* Function of fight with user and robot values */
+
+let fightResult = (userChoice, compChoice) => {
+    nbGameCount++;
+    if((userChoice == "paper") && (compChoice == "paper")) {
+        nbEgalityCount++;
+        return "equality";
+    }
+    else if((userChoice == "paper") && (compChoice == "rock")) {
+        victoryUserCount++;
+        return "win";
+    }
+    else if((userChoice == "paper") && (compChoice == "scissors")) {
+        victoryCompCount++;
+        return "loose";
+    }
+    else if((userChoice == "rock") && (compChoice == "paper")) {
+        victoryCompCount++;
+        return "loose";
+    }
+    else if((userChoice == "rock") && (compChoice == "rock")) {
+        nbEgalityCount++;
+        return "equality";
+    }
+    else if((userChoice == "rock") && (compChoice == "scissors")) {
+        victoryUserCount++;
+        return "win";
+    }
+    else if((userChoice == "scissors") && (compChoice == "paper")) {
+        victoryUserCount++;
+        return "win";
+    }
+    else if((userChoice == "scissors") && (compChoice == "rock")) {
+        victoryCompCount++;
+        return "loose";
+    }
+    else if((userChoice == "scissors") && (compChoice == "scissors")) {
+        nbEgalityCount++;
+        return "equality";
+    }
+    else {
+        console.log("Problème dans les choix");
+    }
+};
+
+/* Robot choice selection random */
+
+let robotRandom = () => {
+    switch( Math.floor((Math.random() * 3))) {
+        case 0 : 
+            return "rock";
+        break;
+        case 1 : 
+            return "paper";
+            break;
+        case 2 : 
+            return "scissors";
+            break;      
+    }
+};
 
 /* Initialize the popover */
 
@@ -23,23 +83,12 @@ pChoice.forEach(element => {
         duration: 1000,
         iterations: Infinity
     });
+
+    element.addEventListener('click', () => {
+        console.log(fightResult(element.id, robotRandom()));
+    });
 });
 
-/* Player choose one of three */
-
-pChoice.forEach((key, element) => {
-
-    //console.log(key)
-    //console.log(element)
+/* Display dynamically the score of game */
 
 
-    //key.addEventListener('click', (element) => {
-
-        //element.parentNode.children[key]
-
-        //console.log(element.parentNode.children[key])
-
-
-        //console.log(element.parentNode.children.)
-    //});
-});
